d47eb5285ed6d9405c1cd2650a08d271b3a81a0f
CodingWithLaura <76746482+CodingWithLaura@users.noreply.github.com>
2021-01-22 15:56:03 +0100

    Add files via upload
    Little JavaScript code for total beginners with a good movie-taste ;)

---
+++ jsyeah.js
@@ -0,0 +1,36 @@
+alert('hell yeah!');
+var b = 'donut';
+console.log(b);
+var a = 'doughnut';
+console.log('no its ' + a);
+//document.getElementById('someid').innerHTML = 'What';
+//document.getElementById('anotherid').innerHTML = 'The Fuck';
+
+var mood = prompt('How do you feel today Dr. Falken?');
+console.log(mood);
+var game = prompt('Would you like to play a game?');
+console.log(game);
+var whichgame = prompt('How about global thermonuclear war?');
+console.log(whichgame);
+
+
+document.getElementById('someid').innerHTML = mood;
+
+//Create a function
+
+function fun() {
+    alert('This is a function you stupid!');
+}
+
+//Call the function
+
+fun();
+
+function hello() {
+    var name = prompt('What is your name?');
+    alert('Konichiwa ' + name + 'san');
+}
+
+hello();
+
+
